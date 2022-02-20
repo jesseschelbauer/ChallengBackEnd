5fe62bec06bca9a29a2a75fe01a6922457a27919
jesseschelbauer <jesseschelbauer@gmail.com>
2022-02-19 23:03:59 -0300

    Added cors support

---
+++ SimpleWebAplication/EndpointsDefinitions/CorsEndpointDefinition.cs
@@ -0,0 +1,23 @@
+﻿namespace SimpleWebAplication.EndpointsDefinitions;
+public class CorsEndpointDefinition : IEndpointDefinition
+{
+    public void Define(WebApplication web)
+    {
+        web.UseCors();
+    }
+
+    public void DefineServices(IServiceCollection services)
+    {
+        services.AddCors(options => {
+            options.AddDefaultPolicy(
+                builder => {
+                    builder.
+                        AllowAnyOrigin()
+                        .AllowAnyHeader()
+                        .AllowAnyMethod();
+                });
+        });
+    }
+
+    public EndpointDefinitionRegistrationPriority RegisterPrior { get { return EndpointDefinitionRegistrationPriority.BeforeAuth; } }
+}
